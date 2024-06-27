de60ced46e9360fe7425d35a0919f0ae1fe8268d
Abror <boqiyerabror@gmail.com>
2024-06-27 22:34:40 +0500

    addad models

--- savdo/blog/models.py
+++ savdo/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+
+    
+    class Meta:
+        ordering = ('date_posted',)
