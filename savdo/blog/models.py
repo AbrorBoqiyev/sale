2057d6699733e26a33dc852dcc11764132022ee4
Abror <boqiyerabror@gmail.com>
2024-06-28 20:01:20 +0500

    blog forms

--- savdo/blog/models.py
+++ savdo/blog/models.py
@@ -6,7 +6,9 @@ class Blog(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
     date_posted = models.DateTimeField(auto_now_add=True)
-
+    
+    def __str__(self) -> str:
+        return self.title
     
     class Meta:
         ordering = ('date_posted',)
--- savdo/blog/views.py
+++ savdo/blog/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from .models import Blog
+# from .forms import BlogsForm
 
 
 
 def blog(request):
+    all_blogs  = Blog.objects.all()
     context = {
         'title': 'This is the blog page',
-        'description': 'hey I am describing this'
+        'description': 'hey I am describing this',
+        'blogs': all_blogs
     }
     return render(request, 'blog.html', context)
