e4f371c1b43361fc73b2bdf6fc425c7435cd1942
Abror <boqiyerabror@gmail.com>
2024-06-29 19:45:50 +0500

    modified color and url

--- savdo/blog/urls.py
+++ savdo/blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('/', blogsview, name='blog'),
+    path('', blogsview, name='blog'),
     path('create_blog', create_blog, name='create_blog'),
 ]
 
