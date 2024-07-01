d8e3a6f9f8231c4028f6ab23d754ae681d5c7154
Abror <boqiyerabror@gmail.com>
2024-07-01 22:00:02 +0500

    CRUD

--- savdo/posts/urls.py
+++ savdo/posts/urls.py
@@ -4,4 +4,8 @@ from .views import *
 
 urlpatterns = [
     path('', postsView, name='posts' ),
+    path('create_post', create_post, name='create_post'),
+    path('update-post', update_post, name='update-post'),
+    path('view_post/<int:pk>', view_post, name='view_post'),
+    path('delete-post/<int:pk>', delete_post, name='delete-post')
 ]
--- savdo/posts/views.py
+++ savdo/posts/views.py
@@ -5,7 +5,38 @@ from .models import Posts
 
 def postsView(request):
     all_posts = Posts.objects.all()
-    context = {
-        'items': all_posts
-    }
-    return render(request, 'posts.html', context)
+    return render(request, 'posts.html', context={'posts': all_posts})
+
+
+def create_post(request):
+    if request.method == 'POST':
+        form = PostsForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('posts')
+    else: 
+        form = PostsForm()
+    return render(request, 'create_post.html', context={'form': form})
+
+
+def update_post(request, pk):
+    post = Posts.objects.get(id=pk)
+    form = PostsForm(instance=post)
+    
+    if request.method == 'POST':
+        form = PostsForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('posts')
+    return render(request, 'update-post.html', context={'form': form})
+
+
+def view_post(request, pk):
+    post = Posts.objects.get(id=pk)
+    return render(request, 'view_post.html', context={'post': post})
+    
+
+def delete_post(request, pk):
+    post = Posts.objects.get(id=pk)
+    post.delete()
+    return redirect('posts')
