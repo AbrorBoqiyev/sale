37a9e552b2e680897a262e8b0e8de326d8cd77cb
Abror <boqiyerabror@gmail.com>
2024-07-04 18:06:38 +0500

    user - updates

--- savdo/users/forms.py
+++ savdo/users/forms.py
@@ -1,26 +1,12 @@
 from django import forms
 
-CHOICES = (
-    ('1', 'Male'),
-    ('2', 'Female'),
-    ('3', 'Other'),
-)
 
 class UserForm(forms.Form):
     username = forms.CharField(label='Username', max_length=50,
                                widget=forms.TextInput(attrs={'placeholder': 'Username'}))
-    first_name = forms.CharField(label='First Name', max_length=50,
-                                 widget=forms.TextInput(attrs={'placeholder': "First name"}))
-    last_name = forms.CharField(label='Last Name', max_length=50,
-                                widget=forms.TextInput(attrs={'placeholder': 'Last Name'}))
-    age = forms.IntegerField(label='age', 
-                             widget=forms.NumberInput(attrs={'placeholder': 'age'}))
     email = forms.CharField(label="email", max_length=100,
-                            widget=forms.TextInput(attrs={'placehlder': "email"}))                            
-    choice = forms.ChoiceField(choices=CHOICES, label='Gender',
-                               widget=forms.Select(attrs={'class': 'form-select'}))
-    # image = forms.ImageField(label='image', widget=forms.FileInput(
-    #                            attrs={'class': 'form-control'}))                               
-    
-    def is_valid(self) -> bool:
-        return super().is_valid()
+                            widget=forms.TextInput(attrs={'placeholder': "email"}))                            
+    password = forms.CharField(label='Password', max_length=50,
+                               widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+    password2 = forms.CharField(label='Confirm Passwrod', max_length=50,
+                                widget=forms.PasswordInput(attrs={'placeholder': 'Confirm Password'}))
--- savdo/users/models.py
+++ savdo/users/models.py
@@ -2,14 +2,23 @@ from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
 
+
+CHOICES = (
+    ('1', 'Male'),
+    ('2', 'Female'),
+    ('3', 'Other'),
+)
+
 # Create your models here.
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    age =  models.IntegerField( null=True, blank=True)
-    gender = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=100, default='')
+    last_name = models.CharField(max_length=100, default='')
     bio = models.TextField(null=True, blank=True)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    age =  models.IntegerField( null=True, blank=True)
+    gender = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
         return f'{self.user.username} Profile'
@@ -17,7 +26,7 @@ class Profile(models.Model):
     def save(self, *args, **kwargs):
         super.save(*args, **kwargs)
          
-        img = Image.open(self.image.path)
+        img = Image.open(self.image.path).convert('RGB')
         if img.height > 300 and img.width > 300:
             output_size = (300, 300)
             img.thumbnail(output_size)
