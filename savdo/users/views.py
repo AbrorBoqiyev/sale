34eecc28671673c8bb51b8d0b8aa7e8325c2a09e
Abror <boqiyerabror@gmail.com>
2024-07-06 08:48:14 +0500

    solution of errs

--- savdo/users/views.py
+++ savdo/users/views.py
@@ -57,5 +57,5 @@ def login_user(request):
 
 def logout_user(request):
     logout(request)
-    messages.success('You have been logged out')
+    messages.success(request, 'You have been logged out')
     return redirect('home')
