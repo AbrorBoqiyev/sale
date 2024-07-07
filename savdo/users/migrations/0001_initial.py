acb7f6b6a0f5949fc30ae33573a6ac10147b05f3
Abror <boqiyerabror@gmail.com>
2024-07-07 22:25:12 +0500

    anredy changes

--- savdo/users/migrations/0001_initial.py
+++ savdo/users/migrations/0001_initial.py
@@ -24,5 +24,6 @@ class Migration(migrations.Migration):
                 ('image', models.ImageField(default='default.jpg', upload_to='profile_pics')),
                 ('user', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
+            
         ),
     ]
