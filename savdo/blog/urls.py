851954b3682ad19ab2698d1d1230f1515f110557
Abror Boqiyev <Boqiyevabror@gmail.com>
2024-07-25 14:10:21 +0500

    renewed

--- savdo/blog/urls.py
+++ savdo/blog/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('delete-blog/<int:pk>', delete_blog, name='delete-blog'),
     
     path('', blog_view, name="blog"),
-    path('api-blog-class/<int:pk>', BlogView.as_view, name="view-blog-class"),
+    path('api-blog-class/<int:pk>', BlogView.as_view(), name="view-blog-class"),
     path('api-blog-class', BlogView.as_view(), name='blog-class'),
     path('api-update-blog/<int:pk>', BlogView.as_view(), name='update-blog-class'),
 ]
