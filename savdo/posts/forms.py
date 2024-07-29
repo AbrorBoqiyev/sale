2d80ab234dcef8ba9fc2d0991c1c68c5639e9e38
Abror Boqiyev <Boqiyevabror@gmail.com>
2024-07-29 22:50:20 +0500

    addition2 to profile page

--- savdo/posts/forms.py
+++ savdo/posts/forms.py
@@ -3,6 +3,11 @@ from .models import *
 
 
 class PostsForm(forms.ModelForm):
+    title = forms.CharField(help_text="Title of your post",widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'Title'}))
+    content = forms.CharField(help_text="Write about your post and your post content", widget=forms.Textarea(
+        attrs={'class': 'form-control', 'placeholder': 'Give some ideas here...'}))
+   
     
     class Meta():
         model = Posts
