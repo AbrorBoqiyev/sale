4b3cc92a46db39bfa0d15d36d4b89e224d9d3690
Abror Boqiyev <Boqiyevabror@gmail.com>
2024-08-15 11:43:41 +0500

    created react-app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import Blogs from "./Blogs";
+
+function App(props) {
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+      <Blogs />
+    </div>
+  );
+}
+
+export default App;
